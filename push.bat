1afa70107cdbb59bf24b86b4810697aea47ba91d
eofdred <muratdogusan@gmail.com>
2026-05-16 10:09:48 +0300

    Migration: updated push scripts for MURAT vault

--- push.bat
+++ push.bat
@@ -1,4 +1,5 @@
 @echo off
+cd /d %~dp0
 title Push Thinking Bottle to GitHub
 cls
 echo ==================================================
--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 
 # Thinking Bottle Git Bash Push Script
 clear
